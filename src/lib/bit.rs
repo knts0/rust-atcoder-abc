d7ab4bbea504ea7498a4b9dd015c6fa47ce8d5ca
Kana Otawara <shimmer22.0204@gmail.com>
2022-05-01 16:41:44 +0900

    add bit_sub

--- src/lib/bit.rs
+++ src/lib/bit.rs
@@ -11,3 +11,20 @@ fn bit() {
         }
     }
 }
+
+fn bit_sub() {
+    let a = 0b10101100;
+    let bit_digits = 8;
+    let mut bit = a;
+    loop {
+        for i in 0..bit_digits {
+            if bit >> i & 1 == 1 {
+                // 列挙
+            }
+        }
+
+        if bit == 0 { break };
+
+        bit = (bit - 1) & a;
+    }
+}
